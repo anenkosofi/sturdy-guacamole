1cdec84e42f2e9843c398802d1971b145ed51b6e
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-07-14 07:54:56 +0300

    fix: change persist

--- src/store/beers/slice.ts
+++ src/store/beers/slice.ts
@@ -34,87 +34,85 @@ export interface BeersState {
 
 export const useBeersStore = create<BeersState>()(
   persist(
-    devtools(
-      set => ({
-        beers: [],
-        isLoading: false,
-        error: null,
-        details: null,
-        selectedBeers: [],
-        lastBeerId: START_ID,
-        visibleBeers: [],
-        page: INITIAL_PAGE,
-        toggleBeer: id => {
-          set(state => ({
-            selectedBeers: state.selectedBeers.includes(id)
-              ? state.selectedBeers.filter(beer => beer !== id)
-              : [id, ...state.selectedBeers],
-          }));
-        },
-        deleteSelectedBeers: () => {
-          set(state => ({
-            beers: state.beers.filter(({ id }) => !state.selectedBeers.includes(id)),
-            visibleBeers: state.visibleBeers.filter(({ id }) => !state.selectedBeers.includes(id)),
-            selectedBeers: [],
-          }));
-        },
-        setPage: page => {
-          set({ page });
-        },
-        setLastBeerId: id => {
-          set({ lastBeerId: id });
-        },
-        setVisibleBeers: () => {
-          set(state => {
-            const isSequentialIds = state.beers.every((beer, index) => beer.id === index + NEXT_ID);
-            const lastBeerIndex = state.beers.findIndex(beer => beer.id === state.lastBeerId);
-            const startIndex = lastBeerIndex === -1 ? START_INDEX : lastBeerIndex + NEXT_INDEX;
-            const endIndex = startIndex + INTERCHANGEABLE_NUMBER;
-            const newVisibleBeers =
-              state.visibleBeers.length < LIMIT_PER_PAGE && !isSequentialIds
-                ? state.beers.slice(
-                    startIndex,
-                    startIndex + LIMIT_PER_PAGE - state.visibleBeers.length
-                  )
-                : state.beers.slice(startIndex, endIndex);
-            return {
-              visibleBeers:
-                state.visibleBeers.length === LIMIT_PER_PAGE
-                  ? [...state.visibleBeers.slice(INTERCHANGEABLE_NUMBER), ...newVisibleBeers]
-                  : [...state.visibleBeers, ...newVisibleBeers],
-            };
-          });
-        },
-        getBeers: async () => {
-          set({ isLoading: true });
-          try {
-            const response = await axios.get(`beers?page=${useBeersStore.getState().page}`);
-            const nextBeers = await response.data;
-            set(state => ({ ...state, beers: [...state.beers, ...nextBeers], isLoading: false }));
-          } catch (e: unknown) {
-            if (e instanceof Error) {
-              set({ error: e.message, isLoading: false });
-            } else {
-              set({ error: 'An unknown error occurred.', isLoading: false });
-            }
-          }
-        },
-        getBeerById: async id => {
-          set(() => ({ isLoading: true }));
-          try {
-            const response = await axios.get(`beers/${id}`);
-            set({ details: await response.data[0], isLoading: false });
-          } catch (e: unknown) {
-            if (e instanceof Error) {
-              set({ error: e.message, isLoading: false });
-            }
+    devtools(set => ({
+      beers: [],
+      isLoading: false,
+      error: null,
+      details: null,
+      selectedBeers: [],
+      lastBeerId: START_ID,
+      visibleBeers: [],
+      page: INITIAL_PAGE,
+      toggleBeer: id => {
+        set(state => ({
+          selectedBeers: state.selectedBeers.includes(id)
+            ? state.selectedBeers.filter(beer => beer !== id)
+            : [id, ...state.selectedBeers],
+        }));
+      },
+      deleteSelectedBeers: () => {
+        set(state => ({
+          beers: state.beers.filter(({ id }) => !state.selectedBeers.includes(id)),
+          visibleBeers: state.visibleBeers.filter(({ id }) => !state.selectedBeers.includes(id)),
+          selectedBeers: [],
+        }));
+      },
+      setPage: page => {
+        set({ page });
+      },
+      setLastBeerId: id => {
+        set({ lastBeerId: id });
+      },
+      setVisibleBeers: () => {
+        set(state => {
+          const isSequentialIds = state.beers.every((beer, index) => beer.id === index + NEXT_ID);
+          const lastBeerIndex = state.beers.findIndex(beer => beer.id === state.lastBeerId);
+          const startIndex = lastBeerIndex === -1 ? START_INDEX : lastBeerIndex + NEXT_INDEX;
+          const endIndex = startIndex + INTERCHANGEABLE_NUMBER;
+          const newVisibleBeers =
+            state.visibleBeers.length < LIMIT_PER_PAGE && !isSequentialIds
+              ? state.beers.slice(
+                  startIndex,
+                  startIndex + LIMIT_PER_PAGE - state.visibleBeers.length
+                )
+              : state.beers.slice(startIndex, endIndex);
+          return {
+            visibleBeers:
+              state.visibleBeers.length === LIMIT_PER_PAGE
+                ? [...state.visibleBeers.slice(INTERCHANGEABLE_NUMBER), ...newVisibleBeers]
+                : [...state.visibleBeers, ...newVisibleBeers],
+          };
+        });
+      },
+      getBeers: async () => {
+        set({ isLoading: true });
+        try {
+          const response = await axios.get(`beers?page=${useBeersStore.getState().page}`);
+          const nextBeers = await response.data;
+          set(state => ({ ...state, beers: [...state.beers, ...nextBeers], isLoading: false }));
+        } catch (e: unknown) {
+          if (e instanceof Error) {
+            set({ error: e.message, isLoading: false });
+          } else {
             set({ error: 'An unknown error occurred.', isLoading: false });
           }
-        },
-      }),
-      {
-        name: 'beers',
-      }
-    )
+        }
+      },
+      getBeerById: async id => {
+        set(() => ({ isLoading: true }));
+        try {
+          const response = await axios.get(`beers/${id}`);
+          set({ details: await response.data[0], isLoading: false });
+        } catch (e: unknown) {
+          if (e instanceof Error) {
+            set({ error: e.message, isLoading: false });
+          }
+          set({ error: 'An unknown error occurred.', isLoading: false });
+        }
+      },
+    })),
+    {
+      name: 'beers',
+    }
   )
 );
